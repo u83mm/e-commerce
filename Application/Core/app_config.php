52baeab9338d8f40da225ca1b63270960d3583ed
Mario <cursotecnoweb@gmail.com>
2024-01-03 22:52:42 +0100

    Updated 'app_config.php' file

--- Application/Core/app_config.php
+++ Application/Core/app_config.php
@@ -6,7 +6,7 @@
 	require_once(SITE_ROOT . '/../vendor/autoload.php');
 	
 	/** Define path to save uploaded images files */
-	define('STORAGE_IMAGES_PATH', SITE_ROOT . "/../uploads/images");
+	define('STORAGE_IMAGES_PATH', SITE_ROOT . "/uploads/images");
 
 	session_start();
 	session_regenerate_id();
