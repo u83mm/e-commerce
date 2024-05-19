593ce02b18348603385ee9e9227752bc9202c5ef
Mario <cursotecnoweb@gmail.com>
2024-05-20 01:39:07 +0200

    Implemented add products to cart

---
+++ Application/Controller/cart/CartController.php
@@ -0,0 +1,116 @@
+<?php
+    declare(strict_types=1);
+
+    use App\Core\Controller;
+    use model\classes\Query;
+    use model\classes\Validate;
+
+    class CartController extends Controller
+    {
+        
+        public function index()
+        {              
+            try {
+                // Test privileges
+                if(!$this->testAccess([
+                    'ROLE_USER',
+                    'ROLE_ADMIN'
+                ])) {
+                    header('Location: /login');
+                    die;
+                }
+
+                $this->render('cart/index_view.twig', [
+                    'menus'         =>  $this->showNavLinks(),
+                    'session'       =>  $_SESSION,                        
+                    'active'        =>  'catalog'
+                ]);
+
+            } catch (\Throwable $th) {
+                $error_msg = [
+                    'error' =>  $th->getMessage(),
+                ];
+
+                if(isset($_SESSION['role']) && $this->testAccess(['ROLE_ADMIN'])) {
+                    $error_msg = [
+                        "Message:"  =>  $th->getMessage(),
+                        "Path:"     =>  $th->getFile(),
+                        "Line:"     =>  $th->getLine(),
+                    ];
+                }
+
+                $this->render('error_view.twig', [
+                    'menus'             => $this->showNavLinks(),
+                    'exception_message' => $error_msg,                
+                ]);
+            }          
+            
+        }
+
+        public function add(string $id = "") 
+        {
+            $validate = new Validate();
+            $query = new Query();            
+
+            try {
+                // Test privileges
+                if(!$this->testAccess([
+                    'ROLE_USER',
+                    'ROLE_ADMIN'
+                ])) {
+                    header('Location: /login');
+                    die;
+                }
+
+                if($_SERVER['REQUEST_METHOD'] == "POST") {
+                    $fields = [
+                        'product_id' => $id,
+                        'quantity'   => $validate->test_input($_POST['quantity']),
+                    ];
+
+                    $product = $query->selectOneBy('products', 'id', $fields['product_id']);
+                    $product['quantity'] = $fields['quantity'];                    
+
+                    if($validate->validate_form($fields)) {                                              
+                        $_SESSION['cart'][] = $product;                      
+
+                        $this->render('products/show_product_view.twig', [
+                            'menus'         =>  $this->showNavLinks(),
+                            'session'       =>  $_SESSION,                        
+                            'active'        =>  'catalog',
+                            'message'       =>  'Product added to cart',
+                            'product'       =>  $product
+                        ]);
+                    }
+                    else {
+                        $this->render('products/show_product_view.twig', [
+                            'menus'         =>  $this->showNavLinks(),
+                            'session'       =>  $_SESSION,                        
+                            'active'        =>  'catalog',
+                            'error_message' =>  $validate->get_msg(),
+                            'product'       =>  $product
+                        ]);
+                    }
+                }                                
+
+            } catch (\Throwable $th) {
+                $error_msg = [
+                    'error' =>  $th->getMessage(),
+                ];
+
+                if(isset($_SESSION['role']) && $this->testAccess(['ROLE_ADMIN'])) {
+                    $error_msg = [
+                        "Message:"  =>  $th->getMessage(),
+                        "Path:"     =>  $th->getFile(),
+                        "Line:"     =>  $th->getLine(),
+                    ];
+                }
+
+                $this->render('error_view.twig', [
+                    'menus'             => $this->showNavLinks(),
+                    'exception_message' => $error_msg,                
+                ]);
+            }
+        }
+    }    
+?>
