2ed4426a771164ed69ae8e831030fb7449d60460
Mario <cursotecnoweb@gmail.com>
2024-05-26 23:01:38 +0200

    Fix issue on showing products in ['cart']

--- Application/Core/app_config.php
+++ Application/Core/app_config.php
@@ -1,12 +1,15 @@
-<?php	
-	session_start();
-	session_regenerate_id();
-
+<?php		
 	define("SITE_ROOT", $_SERVER['DOCUMENT_ROOT']);
 	define('URL', $_SERVER['REQUEST_URI']);		
 
 	require_once(SITE_ROOT . '/../vendor/autoload.php');
-	
+
+	/** Configure directories to load their classes */
+	model\classes\Loader::init(SITE_ROOT . "/..");
+		
 	/** Define path to save uploaded images files */
-	define('STORAGE_IMAGES_PATH', SITE_ROOT . "/uploads/images");		
+	define('STORAGE_IMAGES_PATH', SITE_ROOT . "/uploads/images");
+	
+	session_start();
+	session_regenerate_id();
 ?>
