ba1314ae24c3858ae1f2186e7934bbd248aaa627
Mario Moreno <cursotecnoweb@gmail.com>
2026-02-24 22:56:07 +0100

    feat: update login controller

--- Application/model/classes/AccessControl.php
+++ Application/model/classes/AccessControl.php
@@ -5,10 +5,17 @@
 
     trait AccessControl
     {
-        public function testAccess(array $roles = []) : bool {
+        public function testAccess(array $roles = []) : bool 
+        {
             if(!isset($_SESSION['role'])) return false;
 
             return in_array($_SESSION['role'], $roles) ? true : false;            
         }
-    }    
+
+        public function clearSessionMessages():void 
+        {
+            if(isset($_SESSION['message'])) unset($_SESSION['message']);
+            if(isset($_SESSION['error_message'])) unset($_SESSION['error_message']);
+        }
+    }        
 ?>
